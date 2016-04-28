b5ee9eed65c5bce0716dc153bfc235f358fd2f73
Bharath <reddysbharath@gmail.com>
2016-04-28 00:18:58 -0600

    added mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:          'smtp.gmail.com',
-    port:             2525,
-    domain:           'localhost',
+    port:             587,
+    domain:           'gradta.herokuapp.com',
     user_name:        'bharathcapgemini@gmail.com',
     password:         '.27abkr478',
     authentication:   'plain',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gradta.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:          'smtp.gmail.com',
-    port:             2525,
+    address:          ENV['MAILGUN_SMTP_SERVER'],
+    port:             ENV['MAILGUN_SMTP_PORT'],
     domain:           'gradta.herokuapp.com',
-    user_name:        'bharathcapgemini@gmail.com',
-    password:         '.27abkr478',
+    user_name:        ENV['MAILGUN_SMTP_LOGIN'],
+    password:         ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:   'plain',
     enable_starttls_auto: true 
   }
