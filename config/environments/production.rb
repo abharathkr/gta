c596de9624b27878acf10dfbd133a3f679c0eb69
Bharath <reddysbharath@gmail.com>
2016-04-29 14:03:22 -0600

    live1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'gradta.herokuapp.com' }  
+  config.action_mailer.default_url_options = { host: '159.203.217.54' }  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:          'smtp.gmail.com',
     port:             587,
-    domain:           'gradta.herokuapp.com',
+    domain:           '159.203.217.54',
     user_name:        'bharathcapgemini@gmail.com',
     password:         '.27abkr478',
     authentication:   'plain',
